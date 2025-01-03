9b560429648f23de4655aa4bfa6ffc4e5ec70d60
akambopu <contact@aleixkambou.com>
2025-01-03 12:39:48 -0500

    commented conflicting code

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -2,13 +2,24 @@
 
 # Initialize MySQL directories
 mkdir -p /var/run/mysqld
-chown -R mysql:mysql /var/run/mysqld
+chown -R mysql:mysql /var/run/mysqld /var/lib/mysql
 chmod 777 /var/run/mysqld
 
 # Initialize the MySQL data directory
 if [ ! -d "/var/lib/mysql/mysql" ]; then
     mysql_install_db --user=mysql --datadir=/var/lib/mysql
 fi
+
+# Start MySQL with mysqld_safe
+mysqld_safe --datadir=/var/lib/mysql &
+
+# the code above works needs to go on re - down - up; becuase
+# the build is shit at the moment
+
+# # Wait for MySQL to be ready
+# until mysqladmin ping >/dev/null 2>&1; do
+#     sleep 1
+# done
 
 # Setup database and users
 mysql -u root << EOF
@@ -19,9 +30,8 @@ GRANT ALL PRIVILEGES ON ${db_name}.* TO '${db_user}'@'%';
 FLUSH PRIVILEGES;
 EOF
 
-# Kill temporary MySQL
-pkill mysqld
-sleep 5
+# Stop mysqld_safe
+mysqladmin -u root shutdown
 
 # Start MySQL normally
 exec mysqld --user=mysql
